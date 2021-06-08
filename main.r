0343252b17972f5ec35f8328c9fcbe6f98f527d2
Kasper Rosenkrands <krosenkrands7@gmail.com>
2021-06-08 11:00:46 +0200

    linear, ridge and lasso with no cross validation

---
+++ main.r
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(glmnet)
+set.seed(123)
+
+train.ids <- sample(1:nrow(mtcars), round(nrow(mtcars)*.8), replace = FALSE) 
+
+train <- mtcars[train.ids,]
+test <- mtcars[-train.ids,]
+
+linear.model <- lm(mpg ~ ., data = train)
+linear.pred <- list(
+  train = predict(linear.model, train),
+  test = predict(linear.model, test)
+)
+
+ridge.model <- glmnet(as.matrix(select(train,-mpg)), as.matrix(select(train,mpg)), alpha = 0, lambda = .1)
+ridge.pred <- list(
+  train = predict(ridge.model, as.matrix(select(train, -mpg))),
+  test = predict(ridge.model, as.matrix(select(test, -mpg)))
+)
+
+lasso.model <- glmnet(as.matrix(select(train,-mpg)), as.matrix(select(train,mpg)), alpha = 1, lambda = .1)
+lasso.pred <- list(
+  train = predict(lasso.model, as.matrix(select(train, -mpg))),
+  test = predict(lasso.model, as.matrix(select(test, -mpg)))
+)
+
+pred.data <- tibble(real = test$mpg, pred = linear.pred$test, model = 'linear', data.set = 'test') %>%
+  bind_rows(tibble(real = test$mpg, pred = ridge.pred$test, model = 'ridge', data.set = 'test')) %>%
+  bind_rows(tibble(real = test$mpg, pred = lasso.pred$test, model = 'lasso', data.set = 'test')) %>%
+  bind_rows(tibble(real = train$mpg, pred = linear.pred$train, model = 'linear', data.set = 'train')) %>%
+  bind_rows(tibble(real = train$mpg, pred = ridge.pred$train, model = 'ridge', data.set = 'train')) %>%
+  bind_rows(tibble(real = train$mpg, pred = lasso.pred$train, model = 'lasso', data.set = 'train')) %>%
+  mutate(model = factor(model, levels = c('linear', 'ridge', 'lasso'))) %>%
+  mutate(data.set = factor(data.set, levels = c('train', 'test')))
+
+pred.data %>%  
+  ggplot(aes(x = real, y = pred)) +
+    geom_abline(size = 1) +
+    geom_point(aes(color = model, alpha = data.set)) +
+    theme_bw() +
+    labs(x = 'Observed values', y = 'Predicted values')
+
+pred.performance <- pred.data %>%
+  group_by(model, data.set) %>%
+  summarise(mae = mean(abs(real - pred)))
+
+pred.performance %>%
+  ggplot(aes(x = model, y = mae)) +
+    geom_point(aes(color = data.set)) +
+    theme_bw() +
+    labs(x = 'Model', y='Mean Absolute Error')
